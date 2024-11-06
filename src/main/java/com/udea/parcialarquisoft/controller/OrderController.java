2a89a077074fb90f3812cdced7e436236f1d757b
Rony-san <rony.banol@udea.edu.co>
2024-11-06 10:59:30 -0500

    Exceptions implementation

--- src/main/java/com/udea/parcialarquisoft/controller/OrderController.java
+++ src/main/java/com/udea/parcialarquisoft/controller/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udea.parcialarquisoft.exceptions.RestException;
 import com.udea.parcialarquisoft.model.order.Order;
 import com.udea.parcialarquisoft.model.order.OrderRequest;
 import com.udea.parcialarquisoft.service.OrderService;
@@ -35,38 +36,35 @@ public class OrderController {
     @Operation(summary = "Save a new order")
     @PostMapping
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Order created", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))
-            }),
-    })
-    public ResponseEntity<Order> save(@RequestBody OrderRequest order) {
+        @ApiResponse(responseCode = "201", description = "Order created", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))
+        }),})
+    public ResponseEntity<Order> save(@RequestBody OrderRequest order) throws RestException {
         return new ResponseEntity<>(orderService.saveOrder(order), HttpStatus.CREATED);
     }
 
     //find order by id
     @Operation(summary = "Find order by id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Order found", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))
-            }),
-            @ApiResponse(responseCode = "404", description = "Order not found", content = @Content)
+        @ApiResponse(responseCode = "200", description = "Order found", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))
+        }),
+        @ApiResponse(responseCode = "404", description = "Order not found", content = @Content)
     })
     @GetMapping("/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id) {
+    public ResponseEntity<Order> findById(@PathVariable Long id) throws RestException {
         return new ResponseEntity<>(orderService.findOrderById(id), HttpStatus.OK);
     }
 
     //find all orders
     @Operation(summary = "Find all orders")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Orders found", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))
-            }),
-    })
+        @ApiResponse(responseCode = "200", description = "Orders found", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))
+        }),})
     @GetMapping
     public ResponseEntity<List<Order>> findAll() {
         return new ResponseEntity<>(orderService.findAllOrders(), HttpStatus.OK);
     }
-    
-    
+
 }
